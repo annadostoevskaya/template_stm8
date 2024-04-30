91debd50db78bfc503a8acd4d41617817bdc35e7
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2024-04-30 17:44:32 +0500

    feature: blinky

--- README.txt
+++ README.txt
@@ -4,3 +4,4 @@ References:
 - Bare metal programming: https://lujji.github.io/blog/bare-metal-programming-stm8/
 - SDCC manual: https://sdcc.sourceforge.net/doc/sdccman.pdf
 - CMakeLists.txt: https://github.com/eduardoaugustojulio/8051/
+- STM8 Programming Manual: https://www.st.com/resource/en/programming_manual/pm0044-stm8-cpu-programming-manual-stmicroelectronics.pdf
--- src/main.c
+++ src/main.c
@@ -2,9 +2,40 @@
  * Author: @github.com/annadostoevskaya
  * Filename: main.c
  * Created: 29 Apr 2024 04:08:08 PM
- * Last Update: 30 Apr 2024 12:38:28 AM
+ * Last Update: 30 Apr 2024 05:42:58 PM
  *
  * Description: <EMPTY>
  */
 
-int main() { return 0; }
+#include <stdint.h>
+
+#ifdef __SDCC_stm8
+#define _SFR_(addr) (*(volatile uint8_t *)(0x5000 + (addr)))
+
+/* PORT D */
+#define PD_ODR _SFR_(0x0F)
+#define PD_DDR _SFR_(0x11)
+#define PD_CR1 _SFR_(0x12)
+
+#endif
+
+#define P_LED 4
+
+static inline void delay_ms(uint16_t ms) {
+  for (uint32_t i = 0; i < ((F_CPU / 18000UL) * ms); i += 1) {
+    __asm__("nop");
+  }
+}
+
+int main() {
+  PD_DDR |= (1 << P_LED);
+  PD_CR1 |= (1 << P_LED);
+
+  while (1) {
+    /* toggle pin every 250ms */
+    PD_ODR ^= (1 << P_LED);
+    delay_ms(1000);
+  }
+
+  return 0;
+}
