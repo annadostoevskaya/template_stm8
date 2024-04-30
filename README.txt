7e381df30f94f1068eb561efdedccdc9164d2df1
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2024-04-30 00:27:19 -0700

    feature: README.txt, SDCC toolchain for Win32

---
+++ README.txt
@@ -0,0 +1,5 @@
+# template_stm8
+
+References:
+- Bare metal programming: https://lujji.github.io/blog/bare-metal-programming-stm8/
+- CMakeLists.txt: https://github.com/eduardoaugustojulio/8051/
--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * Author: @github.com/annadostoevskaya
  * Filename: main.c
  * Created: 29 Apr 2024 04:08:08 PM
- * Last Update: 29 Apr 2024 05:55:01 PM
+ * Last Update: 30 Apr 2024 12:26:00 AM
  *
  * Description: <EMPTY>
  */
