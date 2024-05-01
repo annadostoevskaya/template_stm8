9186c5c558f2645accb67c919ec5eabc6f436fdd
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2024-05-01 01:42:36 -0700

    feature: CLK_CKDIVR

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * Author: @github.com/annadostoevskaya
  * Filename: main.c
  * Created: 29 Apr 2024 04:08:08 PM
- * Last Update: 30 Apr 2024 05:42:58 PM
+ * Last Update: 01 May 2024 1:07:32 AM
  *
  * Description: <EMPTY>
  */
@@ -17,6 +17,9 @@
 #define PD_DDR _SFR_(0x11)
 #define PD_CR1 _SFR_(0x12)
 
+/* CLOCK */
+#define CLK_CKDIVR _SFR_(0xC6)
+
 #endif
 
 #define P_LED 4
@@ -28,6 +31,9 @@ static inline void delay_ms(uint16_t ms) {
 }
 
 int main() {
+  // NOTE(annad): Disable prescale for High-Speed-Internal RC
+  // CLK_CKDIVR &= ~(0x18);
+
   PD_DDR |= (1 << P_LED);
   PD_CR1 |= (1 << P_LED);
 
